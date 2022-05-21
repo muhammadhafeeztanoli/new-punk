7a5627b5ab9b28638bef3780c1b3e0027f5b8443
Muhammad Hafeez <58856895+muhammadhafeeztanoli@users.noreply.github.com>
2022-05-21 09:47:52 +0500

    Update App.js
    new changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import './App.css';
-import Maincomponent from './maincomponent';
-import giffile from './put_under_mint_button.gif';
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="row align-center height">
-          <div>
-            <div className="logo">
-              <img className='' src="./logo_cropped.png" />
-            </div>
-          </div>
-          <div className="col-md-12 row_element">
-          <div className="col-md-4 padding_element">
-              <Maincomponent/>
-          </div>
-          </div>
-          <div className="col-md-12 row_element " style={{marginBottom:'20px'}}>
-            <img className="gifstyle" alt='props' src={giffile}/>
-          </div>
-          </div>
-      </header>
-    </div>
-  );
-}
+// import './App.css';
+// import Maincomponent from './maincomponent';
+// import giffile from './put_under_mint_button.gif';
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div className="row align-center height">
+//           <div>
+//             <div className="logo">
+//               <img className='' src="./logo_cropped.png" />
+//             </div>
+//           </div>
+//           <div className="col-md-12 row_element">
+//           <div className="col-md-4 padding_element">
+//               <Maincomponent/>
+//           </div>
+//           </div>
+//           <div className="col-md-12 row_element " style={{marginBottom:'20px'}}>
+//             <img className="gifstyle" alt='props' src={giffile}/>
+//           </div>
+//           </div>
+//       </header>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
